008c5def9021c4aae431dd08da191a950c0b7da5
PIGEONHUSBAND <130369183+PIGEONHUSBAND@users.noreply.github.com>
2023-04-11 19:45:21 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	printf("HELLOE BIT/n");
+//		return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n%d\n", 10, 20);
+//	return 0;
+//}
+//int main()
+//{
+//	int weight1 = 1;
+//	int weight2 = 2;
+//	int weight3 = 3;
+//	int sum = weight1 + weight2 + weight3;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int a = 10;
+//int main()
+//{
+//	int b = 20;
+//	printf("a=%db=%d\n", a, b);
+//	return 0;
+//}
+//int main()
+//{
+//	int num1=0;
+//	int num2=0;
+//	int sum=0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
