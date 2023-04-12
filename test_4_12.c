422206ad5ce91b3b6386c2ff55daae372e28f87c
Cherry Liu Coder <130369183+PIGEONHUSBAND@users.noreply.github.com>
2023-04-12 21:51:45 +0800

    Add files via upload

---
+++ test_4_12.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>;
+
+//int main()
+//{
+//	printf("加入河海大家庭学习\n");
+//	printf("你愿意好好学习吗？");
+//	int flag  =  0;
+//	scanf("%d", &flag);
+//	if (flag == 1)
+//		printf("好offer\n");
+//	else if (flag == 0)
+//		printf("卖红薯\n");
+//	return 0;
+//}
