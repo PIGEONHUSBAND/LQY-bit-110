149836b280358f801c752bd7cd115e73f12e06af
Cherry Liu Coder <130369183+PIGEONHUSBAND@users.noreply.github.com>
2023-04-13 19:54:48 +0800

    Add files via upload

---
+++ test_4_13/test_4_13.c
@@ -0,0 +1,211 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>;
+//int main()
+//{
+//	int a = 555;
+//	/*scanf("%d\n" , &a);*/
+//	printf("%d", a);
+//	return 0;
+//}
+//int main()
+//{
+//	char ch = 0;
+//	printf("%c\n" , ch);
+//	return 0;
+//}
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c" , &ch );
+//	printf("%d" , ch);
+//	return 0;
+//}
+//int main()
+//{
+//	{
+//		int a = 100;//被终止
+//	}
+//	int a = 99;
+//	printf("%d" , a);
+//	return 0;
+//}
+//int a = 100;
+//
+//void test()
+//{
+//	printf("2--->%d\n" , a);
+//}
+//
+//int main()
+//{
+//	printf("1--->%d\n", a);
+//	test();
+//	return 0;
+//}
+
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n", g_val);
+//	return 0;
+//}
+//int main()
+//{
+//	{
+//		int a = 2;
+//		int b = 3;
+//		int c = a + b;
+//		printf("%d\n" , c);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	printf("%d\n" , a);
+//	a = 5;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//#define SIZE 10
+//#define CH 'W'
+
+//int main()
+//{
+//	int a = SIZE;
+//	char b = CH;
+//	printf ("%d\n" , a);
+//	printf("%c\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[SIZE] = {2};
+//	return 0;
+//}
+//enum SEX
+//{
+//	MALE=2,
+//	FEMALE=4,
+//	SECRECT=6
+//};
+//int main()
+//{
+//	enum SEX;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRECT);
+//	return 0;
+//}
+//int main()
+//{
+//	char ch1[] = "abcdefg";
+//	char ch2[] = { 'a','b','c','d','e','f','g' };
+//	printf("%s\n" , ch1);
+//	printf("%s\n", ch2);
+//	return 0;
+//}
+//int main()
+//{
+//	char ch1[] = { 'a','b','c' };
+//	char ch2[] = "abc";
+//	int len1 = strlen(ch1);
+//	int len2 = strlen(ch2);
+//	printf("%d\n" , len1);
+//	printf("%d\n" , len2);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n" , strlen("abcdef"));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", strlen("abcd\0ef"));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("加入比特学习。");
+//	printf("你愿意好好学习吗？");
+//	int flag = 0;
+//	scanf("%d" , &flag);
+//	if (flag == 1)
+//		printf("好offer！");
+//	else if (flag == 0)
+//		printf("卖红薯。");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	while (line < 50000)
+//	{
+//		printf("敲代码：%d\n", line);
+//		line++;
+//	}
+//	if (line == 50000)
+//		printf("好offer！");
+//	else
+//		printf("差点意思。");
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int M = 0;
+//    scanf("%d", &M);
+//    if (M % 5 == 0)
+//        printf("YES");
+//    else
+//        printf("NO");
+//    return 0;
+//}
+//int main()
+//{
+//    int x = 0;
+//    int y = 0;
+//    scanf("%d", &x);
+//    if (x < 0)
+//        printf("y=1", y);
+//    if (x == 0)
+//        printf("y=0", y);
+//    else if (x > 0)
+//        printf("y=-1", y);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &b, &a);
+//    int c = b / a;
+//    int d = b % a;
+//    printf("%d %d", c, d);
+//    return 0;
+//}
+//int main()
+//{
+//    int seconds = 0;
+//    int h = 0;
+//    int min = 0;
+//    int s = 0;
+//    scanf("%d", &seconds);
+//    h = seconds / 3600;
+//    min = (seconds - h * 3600) / 60;
+//    s = seconds - min * 60 - 3600 * h;
+//    printf("%d %d %d", h, min, s);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 40;
+//    int c = 212;
+//    printf("%d", (-8 + 22) * a - 10 + c / 2);
+//    return 0;
+//}
