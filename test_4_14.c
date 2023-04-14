2ecaf23d2b357502691bd89a25d8f71ce7c6d36d
Cherry Liu Coder <130369183+PIGEONHUSBAND@users.noreply.github.com>
2023-04-14 21:09:25 +0800

    Add files via upload

---
+++ test_4_14.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//¶à×éÊäÈë
+// 
+//int main()
+//{
+//	int iq = 0;
+//	scanf("%d", &iq);
+//	while (scanf("%d", &iq) == 1)
+//	{
+//		if (iq >= 140)
+//			printf("Genius\n");
+//	}
+//	return 0;
+//}
