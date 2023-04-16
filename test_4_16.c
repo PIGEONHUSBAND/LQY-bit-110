f877cd69a573f69bf528a1e4e5110685ca6886b1
Cherry Liu Coder <130369183+PIGEONHUSBAND@users.noreply.github.com>
2023-04-16 23:06:58 +0800

    Add files via upload

---
+++ test_4_16.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int MAX(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	if (x < y)
+//		return y;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int max = MAX(a, b);
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    while (scanf("%d%d%d", &a, &b, &c) != EOF)
+//    {
+//        if (a + b > c && a + c > b && b + c > a && a - b < c && a - c < b && b - c < a)
+//        {
+//            if (a == b && a == c && b == c)
+//            {
+//                printf("%s\n", "Equilateral triangle!");
+//            }
+//
+//            else if (a == b || a == c || b == c)
+//            {
+//                printf("%s\n", "Isosceles triangle!");
+//            }
+//
+//            else
+//            {
+//                printf("%s\n", "Ordinary triangle!");
+//            }
+//
+//        }
+//        else
+//        {
+//            printf("Not a triangle!");
+//        }
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a = printf("Hello world!");
+//    printf("\n%d\n", a);
+//    return 0;
+//}
