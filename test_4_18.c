5462f390a97651cc1658e95a542924ca879ec1f7
Cherry Liu Coder <130369183+PIGEONHUSBAND@users.noreply.github.com>
2023-04-18 18:13:52 +0800

    Add files via upload

---
+++ test_4_18.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//    float w = 0;
+//    float h = 0;
+//    scanf("%f%f", &w, &h);
+//    printf("%.2f", w / ((h / 100) * (h / 100)));
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    int age = 0;
+//    long int s = 3.156e7;
+//    scanf("%d", &age);
+//    printf("%ld", age * s);
+//    return 0;
+//}
